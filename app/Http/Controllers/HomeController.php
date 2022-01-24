8167da8838c4b967407c5cd0bf91fca9a619bf3a
nemerald-voip <58834187+nemerald-voip@users.noreply.github.com>
2022-01-23 17:46:39 -0800

    updated home controller to point at Fusion PBX index file

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -23,6 +23,7 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        header("Location: ".PROJECT_PATH."/fusionpbx_index.php");
+		exit;
     }
 }
