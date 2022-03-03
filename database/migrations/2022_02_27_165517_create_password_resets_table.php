b1d0d157fd744380a00d0d1659dfa789b2fc0c2c
nemerald-voip <info@nemerald.com>
2022-03-02 23:07:32 -0800

    updated password reset table

--- database/migrations/2022_02_27_165517_create_password_resets_table.php
+++ database/migrations/2022_02_27_165517_create_password_resets_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
 
 class CreatePasswordResetsTable extends Migration
 {
@@ -14,6 +15,7 @@ class CreatePasswordResetsTable extends Migration
     public function up()
     {
         Schema::create('password_resets', function (Blueprint $table) {
+            $table->uuid('id')->primary()->default(DB::raw('uuid_generate_v4()'));
             $table->string('email')->index();
             $table->string('token')->index();
             $table->timestamp('created_at');
