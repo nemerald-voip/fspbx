b178a3e52c22f31b1758e0bd075f0e3a4c26a062
Alexander S <alexander@sharapov.biz>
2023-02-10 16:26:50 +0600

    Making progress on fax queue

--- app/Models/FaxQueues.php
+++ app/Models/FaxQueues.php
@@ -2,13 +2,14 @@
 
 namespace App\Models;
 
+use App\Models\Traits\TraitUuid;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class FaxQueues extends Model
 {
-    use HasFactory, \App\Models\Traits\TraitUuid;
-    
+    use HasFactory, TraitUuid;
+
     protected $table = "v_fax_queue";
 
     public $timestamps = false;
@@ -17,36 +18,5 @@ class FaxQueues extends Model
     public $incrementing = false;
     protected $keyType = 'string';
 
-
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
-    protected $fillable = [
-        'fax_queue_uuid',
-        'domain_uuid',
-        'fax_uuid',
-        'origination_uuid',
-        'fax_date',
-        'hostname',
-        'fax_caller_id_name',
-        'fax_caller_id_number',
-        'fax_number',
-        'fax_prefix',
-        'fax_email_address',
-        'fax_file',
-        'fax_status',
-        'fax_retry_date',
-        'fax_notify_sent',
-        'fax_notify_date',
-        'fax_retry_count',
-        'fax_accountcode',
-        'fax_command',
-        'fax_log_uuid',
-        'insert_date',
-        'insert_user',
-        'update_date',
-        'update_user'
-    ];
+    protected $guarded = ['fax_queue_uuid'];
 }
