0ce6dd9d6eba455defcb10845f894fb76f17f1df
nemerald-voip <info@nemerald.com>
2023-02-12 04:11:56 -0800

    fax changes

---
+++ resources/views/layouts/partials/listing/layout.blade.php
@@ -0,0 +1,36 @@
+@extends('layouts.horizontal', ["page_title" => $pageTitle])
+
+@section('content')
+    <div class="container-fluid">
+        <div class="row">
+            @include('layouts.partials.subheader', ['title' => $pageTitle])
+        </div>
+        <div class="row">
+            <div class="col-12">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="row mb-3">
+                            <div class="col-xl-8">
+                                @yield('searchbar')
+                            </div>
+                            <div class="col-xl-4">
+                                <div class="text-xl-end mt-xl-0 mt-2">
+                                    @yield('actionbar')
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            @yield('pagination')
+                        </div>
+                        <div class="table-responsive">
+                            <table class="table table-centered mb-0">
+                                <thead class="table-light">@yield('table-head')</thead>
+                                <tbody>@yield('table-body')</tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
