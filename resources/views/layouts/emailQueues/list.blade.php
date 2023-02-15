f771cc6ff905dbed85e827aa5532893f6f3c4741
Sagar Naliyapara <naliyaparaspn@gmail.com>
2023-02-15 12:33:29 +0530

    Open modal button

--- resources/views/layouts/emailQueues/list.blade.php
+++ resources/views/layouts/emailQueues/list.blade.php
@@ -69,9 +69,7 @@
                                 </div>
                             </div>
                         </div>
-                        <button data-toggle="modal" data-target="#exampleModal" data-whatever="test">
-                            Show data
-                        </button>
+                        <button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#exampleModal">Open Modal</button>
 
                         <div class="table-responsive">
                             <table class="table table-centered mb-0" id="voicemail_list">
