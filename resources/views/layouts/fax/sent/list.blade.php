a42e04df50726c623246e2b0021f2afe564856db
Alexander S <alexander@sharapov.biz>
2023-02-27 14:01:09 +0600

    Improving fax sent page

--- resources/views/layouts/fax/sent/list.blade.php
+++ resources/views/layouts/fax/sent/list.blade.php
@@ -1,159 +1,118 @@
-@extends('layouts.horizontal', ["page_title"=> "Sent Faxes"])
+@extends('layouts.partials.listing.layout', ["pageTitle"=> 'Sent Faxes', 'breadcrumbs' => [
+    'Virtual Fax Machines' => 'faxes.index',
+    'Fax Sent' => ''
+]])
 
-@section('content')
-
-<!-- Start Content-->
-<div class="container-fluid">
-
-    <!-- start page title -->
-    <div class="row">
-        <div class="col-12">
-            <div class="page-title-box">
-                <div class="page-title-right">
-                    <ol class="breadcrumb m-0">
-                        <li class="breadcrumb-item"><a href="{{ route('faxes.index') }}">Virtual Fax Machines</a></li>
-                        <li class="breadcrumb-item active">Fax Sent</li>
-                    </ol>
-                </div>
-                <h4 class="page-title">Sent Faxes</h4>
-            </div>
-        </div>
-    </div>
-    <!-- end page title -->
-
-    <div class="row">
-        <div class="col-12">
-            <div class="card">
-                <div class="card-body">
-                    <div class="row mb-2">
-                        <div class="col-xl-4">
-                            <label class="form-label">Showing {{ $files->count() ?? 0 }}  results for sent faxes</label>
-                        </div>
-                        <div class="col-xl-8">
-                            <div class="text-xl-end mt-xl-0 mt-2">
-
-                                @if ($permissions['delete'])
-                                <a href="javascript:confirmDeleteAction('{{ route('faxes.file.deleteFaxFile', ':id') }}');" id="deleteMultipleActionButton" class="btn btn-danger mb-2 me-2 disabled">
-                                    Delete Selected
-                                    </a>
-                                @endif
-                                {{-- <button type="button" class="btn btn-light mb-2">Export</button> --}}
-                            </div>
-                        </div><!-- end col-->
-                    </div>
-
-                    <div class="table-responsive">
-                        <table class="table table-centered mb-0" id="voicemail_list">
-                            <thead class="table-light">
-                                <tr>
-                                    <th style="width: 20px;">
-                                        @if ($permissions['delete'])
-                                            <div class="form-check">
-                                                <input type="checkbox" class="form-check-input" id="selectallCheckbox">
-                                                <label class="form-check-label" for="selectallCheckbox">&nbsp;</label>
-                                            </div>
-                                        @endif
-                                    </th>
-                                    <th>From</th>
-                                    <th>To</th>
-                                    {{-- <th>View</th> --}}
-                                    <th>Date</th>
-                                    <th>Status</th>
-                                    <th>Last attempt</th>
-                                    <th>Retry Count</th>
-                                    <th>Actions</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-
-                                @foreach ($files as $key=>$file)
-                                    <tr id="id{{ $file->fax_file_uuid  }}">
-                                        <td>
-                                            @if ($permissions['delete'])
-                                                <div class="form-check">
-                                                    <input type="checkbox" name="action_box[]" value="{{ $file->fax_file_uuid }}" class="form-check-input action_checkbox">
-                                                    <label class="form-check-label" >&nbsp;</label>
-                                                </div>
-                                            @endif
-                                        </td>
-                                        <td>
-                                            @if($file->fax_caller_id_name!='')
-                                            <span class="text-body fw-bold">
-                                                {{ $file->fax_caller_id_name ?? ''}}
-                                            </span>
-                                            <br>
-                                            @endif
-                                            <span class="text-body fw-bold ">
-                                                {{ $file->fax_caller_id_number ?? '' }}
-                                            </span>
-                                        </td>
-                                        <td>
-                                            {{ $file->fax_destination }}
-                                        </td>
-                                        {{-- <td>
-                                            {{ $file->fax_file_type }}
-                                    </td> --}}
-                                        <td>
-                                            <span class="text-body text-nowrap">{{ $file->fax_date->format('D, M d, Y ')}}</span>
-                                            <span class="text-body text-nowrap">{{ $file->fax_date->format('h:i:s A') }}</span>
-                                        </td>
-                                        <td>
-                                            @if ($file->fax_status == "sent")
-                                                <h5><span class="badge bg-success">Sent</span></h5>
-                                            @elseif($file->fax_status == "failed")
-                                                <h5><span class="badge bg-danger">Failed</span></h5>
-                                            @else
-                                                <h5><span class="badge bg-info">{{ ucfirst($file->fax_status) }}</span></h5>
-                                            @endif
-                                        </td>
-                                        <td>
-                                            @if ($file->fax_retry_date)
-                                                <span class="text-body text-nowrap">{{ $file->fax_retry_date->format('D, M d, Y ') }}</span>
-                                                <span class="text-body text-nowrap">{{ $file->fax_retry_date->format('h:i:s A') }}</span>
-                                            @endif
-                                        </td>
-                                        <td>
-                                            {{ $file->fax_retry_count }}
-                                        </td>
-                                        <td>
-                                            <div id="tooltip-container-actions">
-                                                @if ($file->faxQueue)
-                                                    @if($file->faxQueue->fax_status == 'waiting' or $file->faxQueue->fax_status == 'trying')
-                                                        <a href="{{ route('faxes.file.updateStatus', [$file->faxQueue->fax_queue_uuid]) }}" class="action-icon">
-                                                            <i class="mdi mdi-cancel" data-bs-container="#tooltip-container-actions" data-bs-toggle="tooltip" data-bs-placement="bottom" title="Cancel trying"></i>
-                                                        </a>
-                                                    @else
-                                                        <a href="{{ route('faxes.file.updateStatus', [$file->faxQueue->fax_queue_uuid, 'waiting']) }}" class="action-icon">
-                                                            <i class="mdi mdi-restart" data-bs-container="#tooltip-container-actions" data-bs-toggle="tooltip" data-bs-placement="bottom" title="Retry"></i>
-                                                        </a>
-                                                    @endif
-                                                @endif
-                                                <a href="{{ route('downloadSentFaxFile', $file->fax_file_uuid ) }}" class="action-icon">
-                                                    <i class="mdi mdi-download" data-bs-container="#tooltip-container-actions" data-bs-toggle="tooltip" data-bs-placement="bottom" title="Download"></i>
-                                                </a>
-                                                @if ($permissions['delete'])
-                                                    <a href="javascript:confirmDeleteAction('{{ route('faxes.file.deleteFaxFile', ':id') }}','{{ $file->fax_file_uuid }}');" class="action-icon">
-                                                        <i class="mdi mdi-delete" data-bs-container="#tooltip-container-actions" data-bs-toggle="tooltip" data-bs-placement="bottom" title="Delete"></i>
-                                                    </a>
-                                                @endif
-                                            </div>
-                                        </td>
-                                    </tr>
-                                @endforeach
-
-
-                            </tbody>
-                        </table>
-                    </div>
-                </div> <!-- end card-body-->
-            </div> <!-- end card-->
-        </div> <!-- end col -->
-    </div>
-    <!-- end row -->
-
-</div> <!-- container -->
+@section('pagination')
+    @include('layouts.partials.listing.pagination', ['collection' => $files])
 @endsection
 
+@section('actionbar')
+    @if ($permissions['delete'])
+        <a href="javascript:confirmDeleteAction('{{ route('faxes.file.deleteFaxFile', ':id') }}');" id="deleteMultipleActionButton" class="btn btn-danger mb-2 me-2 disabled">
+            Delete Selected
+        </a>
+    @endif
+    {{-- <button type="button" class="btn btn-light mb-2">Export</button> --}}
+@endsection
+
+@section('table-head')
+    <tr>
+        <th style="width: 20px;">
+            @if ($permissions['delete'])
+                <div class="form-check">
+                    <input type="checkbox" class="form-check-input" id="selectallCheckbox">
+                    <label class="form-check-label" for="selectallCheckbox">&nbsp;</label>
+                </div>
+            @endif
+        </th>
+        <th>From</th>
+        <th>To</th>
+        {{-- <th>View</th> --}}
+        <th>Date</th>
+        <th>Status</th>
+        <th>Last attempt</th>
+        <th>Retry Count</th>
+        <th>Actions</th>
+    </tr>
+@endsection
+
+@section('table-body')
+    @foreach ($files as $key=>$file)
+        <tr id="id{{ $file->fax_file_uuid  }}">
+            <td>
+                @if ($permissions['delete'])
+                    <div class="form-check">
+                        <input type="checkbox" name="action_box[]" value="{{ $file->fax_file_uuid }}" class="form-check-input action_checkbox">
+                        <label class="form-check-label" >&nbsp;</label>
+                    </div>
+                @endif
+            </td>
+            <td>
+                @if($file->fax_caller_id_name!='')
+                    <span class="text-body fw-bold">
+                                                {{ $file->fax_caller_id_name ?? ''}}
+                                            </span>
+                    <br>
+                @endif
+                <span class="text-body fw-bold ">
+                                                {{ $file->fax_caller_id_number ?? '' }}
+                                            </span>
+            </td>
+            <td>
+                {{ $file->fax_destination }}
+            </td>
+            {{-- <td>
+                {{ $file->fax_file_type }}
+        </td> --}}
+            <td>
+                <span class="text-body text-nowrap">{{ $file->fax_date->format('D, M d, Y ')}}</span>
+                <span class="text-body text-nowrap">{{ $file->fax_date->format('h:i:s A') }}</span>
+            </td>
+            <td>
+                @if ($file->fax_status == "sent")
+                    <h5><span class="badge bg-success">Sent</span></h5>
+                @elseif($file->fax_status == "failed")
+                    <h5><span class="badge bg-danger">Failed</span></h5>
+                @else
+                    <h5><span class="badge bg-info">{{ ucfirst($file->fax_status) }}</span></h5>
+                @endif
+            </td>
+            <td>
+                @if ($file->fax_retry_date)
+                    <span class="text-body text-nowrap">{{ $file->fax_retry_date->format('D, M d, Y ') }}</span>
+                    <span class="text-body text-nowrap">{{ $file->fax_retry_date->format('h:i:s A') }}</span>
+                @endif
+            </td>
+            <td>
+                {{ $file->fax_retry_count }}
+            </td>
+            <td>
+                <div id="tooltip-container-actions">
+                    @if ($file->faxQueue)
+                        @if($file->faxQueue->fax_status == 'waiting' or $file->faxQueue->fax_status == 'trying')
+                            <a href="{{ route('faxes.file.updateStatus', [$file->faxQueue->fax_queue_uuid]) }}" class="action-icon">
+                                <i class="mdi mdi-cancel" data-bs-container="#tooltip-container-actions" data-bs-toggle="tooltip" data-bs-placement="bottom" title="Cancel trying"></i>
+                            </a>
+                        @else
+                            <a href="{{ route('faxes.file.updateStatus', [$file->faxQueue->fax_queue_uuid, 'waiting']) }}" class="action-icon">
+                                <i class="mdi mdi-restart" data-bs-container="#tooltip-container-actions" data-bs-toggle="tooltip" data-bs-placement="bottom" title="Retry"></i>
+                            </a>
+                        @endif
+                    @endif
+                    <a href="{{ route('downloadSentFaxFile', $file->fax_file_uuid ) }}" class="action-icon">
+                        <i class="mdi mdi-download" data-bs-container="#tooltip-container-actions" data-bs-toggle="tooltip" data-bs-placement="bottom" title="Download"></i>
+                    </a>
+                    @if ($permissions['delete'])
+                        <a href="javascript:confirmDeleteAction('{{ route('faxes.file.deleteFaxFile', ':id') }}','{{ $file->fax_file_uuid }}');" class="action-icon">
+                            <i class="mdi mdi-delete" data-bs-container="#tooltip-container-actions" data-bs-toggle="tooltip" data-bs-placement="bottom" title="Delete"></i>
+                        </a>
+                    @endif
+                </div>
+            </td>
+        </tr>
+    @endforeach
+@endsection
 
 @push('scripts')
 <script>
