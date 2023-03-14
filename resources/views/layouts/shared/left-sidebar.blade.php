be1b57cd2e77423bd0a74073a5ed410556992ea3
David Kats <davidk@lightningtelecom.com>
2023-03-14 17:12:13 -0400

    changes to logo location

--- resources/views/layouts/shared/left-sidebar.blade.php
+++ resources/views/layouts/shared/left-sidebar.blade.php
@@ -4,7 +4,7 @@
     <!-- LOGO -->
     <a href="{{route('any', 'index')}}" class="logo text-center logo-light">
         <span class="logo-lg">
-            <img src="{{asset('assets/images/logo.png')}}" alt="" height="16">
+            <img src="{{asset('/storage/logo.png')}}" alt="" height="16">
         </span>
         <span class="logo-sm">
             <img src="{{asset('assets/images/logo_sm.png')}}" alt="" height="16">
