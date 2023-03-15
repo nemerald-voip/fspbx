41e8fa01ab60c207cafdc22aaebd199dbcaabfde
David Kats <davidk@lightningtelecom.com>
2023-03-15 14:53:31 -0400

    added a script for easier installation

---
+++ install/queue_server.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+sudo apt-get install php7.4-mbstring
+sudo apt install imagemagick php7.4-imagick
+sudo apt-get install php7.4-zip
+service  php7.4-fpm restart
+echo "PHP dependencies are installed\n\n"
+
+sudo apt-get -y install supervisor
+echo "Supervisor is installed\n\n"
+
+sudo apt-get -y install redis-server
+cp install/redis.conf /etc/redis/redis.conf
+service redis-server restart
+echo "Redis Server is installed\n\n"
+
+
+cp install/horizon.conf /etc/supervisor/conf.d/
+
+sudo supervisorctl reread
+sudo supervisorctl update
+sudo supervisorctl restart horizon:*
+echo "Horizon is installed\n\n"
+
+composer dump-autoload
+
+
+
