4664e2fb8a01a90f9dc318d8c5ba64a120c38a10
nemerald-voip <info@nemerald.com>
2023-04-27 08:17:16 -0700

    new proccess commio

---
+++ app/Jobs/ProcessCommioSMS.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Redis;
+use Illuminate\Queue\SerializesModels;
+use App\Models\Commio\CommioOutboundSMS;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Support\Facades\Notification;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
+use App\Notifications\SendSlackFaxNotification;
+use Illuminate\Queue\Middleware\RateLimitedWithRedis;
+
+class ProcessCommioSMS implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * The number of times the job may be attempted.
+     *
+     * @var int
+     */
+    public $tries = 10;
+
+    /**
+     * The maximum number of unhandled exceptions to allow before failing.
+     *
+     * @var int
+     */
+    public $maxExceptions = 5;
+
+    /**
+     * The number of seconds the job can run before timing out.
+     *
+     * @var int
+     */
+    public $timeout = 120;
+
+    /**
+     * Indicate if the job should be marked as failed on timeout.
+     *
+     * @var bool
+     */
+    public $failOnTimeout = true;
+
+    /**
+     * The number of seconds to wait before retrying the job.
+     *
+     * @var int
+     */
+    public $backoff = 30;
+
+    /**
+     * Delete the job if its models no longer exist.
+     *
+     * @var bool
+     */
+    public $deleteWhenMissingModels = true;
+
+    private $to_did;
+    private $from_did;
+    private $message;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct($data)
+    {
+        $this->to_did = $data['to_did'];
+        $this->from_did = $data['from_did'];
+        $this->message = $data['message'];
+    }
+
+    /**
+     * Get the middleware the job should pass through.
+     *
+     * @return array
+     */
+    public function middleware()
+    {
+        return [(new RateLimitedWithRedis('messages'))];
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        // Allow only 2 tasks every 1 second
+        Redis::throttle('messages')->allow(2)->every(1)->then(function () {
+            
+            $sms = new CommioOutboundSMS();
+            $sms->to_did = $this->to_did;
+            $sms->from_did = $this->from_did;
+            $sms->message = $this->message;
+            $sms->send();
+
+        }, function () {
+            // Could not obtain lock; this job will be re-queued
+            return $this->release(5);
+        });
+    }
+}
