eb000d92f84dec288f47cdc8bc3830ae40528602
nemerald-voip <info@nemerald.com>
2023-04-26 19:32:38 -0700

    Commio SMS project

--- app/Http/Webhooks/SignatureValidators/CommioSignatureValidator.php
+++ app/Http/Webhooks/SignatureValidators/CommioSignatureValidator.php
@@ -10,6 +10,9 @@ class CommioSignatureValidator implements SignatureValidator
 {
     public function isValid(Request $request, WebhookConfig $config) : bool {
         
+        //If there is a way to validate that this request came from Commio then let's implement the logic here
+        // Otherwise it's defauted to athenticate all post requests
+
         return true;
     }
 }
