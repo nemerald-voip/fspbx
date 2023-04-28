e2aa8408f26694e43cc64bf50d625613da5b2bca
nemerald-voip <info@nemerald.com>
2023-04-28 12:59:37 -0700

    final improvements to inbound commio SMS

--- app/Models/Commio/CommioInboundSMS.php
+++ app/Models/Commio/CommioInboundSMS.php
@@ -4,8 +4,9 @@ namespace App\Models\Commio;
 
 use App\Models\Messages;
 use Illuminate\Support\Facades\Log;
-use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Http;
+use Illuminate\Database\Eloquent\Model;
+use App\Jobs\SendSmsNotificationToSlack;
 
 /**
  * @property string|null $domain_setting_value
@@ -16,7 +17,7 @@ use Illuminate\Support\Facades\Http;
  */
 class CommioInboundSMS extends Model
 {
-    public $domain_setting_value;
+    public $org_id;
     public $to_did;
     public $from_did;
     public $message;
@@ -35,7 +36,7 @@ class CommioInboundSMS extends Model
             Log::alert("Could not find sms entity from ".$this->from_did." to ".$this->to_did);
         }
 
-        // Logic to send the SMS message using a third-party Commio API,
+        // Logic to deliver the SMS message using a third-party Ringotel API,
         // This method should return a boolean indicating whether the message was sent successfully.
         $response = Http::ringotel_api()
             //->dd()
@@ -43,7 +44,7 @@ class CommioInboundSMS extends Model
             ->withBody(json_encode([
                 'method' => 'message',
                 'params' => [
-                    'orgid' => $this->domain_setting_value,
+                    'orgid' => $this->org_id,
                     'from' => $this->from_did,
                     'to' => $this->to_did,
                     'content' => $this->message
@@ -51,8 +52,10 @@ class CommioInboundSMS extends Model
             ]), 'application/json')
             ->post('/')
             ->throw(function ($response, $e) {
-                Notification::route('mail', 'dexter@stellarvoip.com')
-                    ->notify(new StatusUpdate("error"));
+                // Notification::route('mail', 'dexter@stellarvoip.com')
+                //     ->notify(new StatusUpdate("error"));
+                Log::alert("Error delivering SMS to Ringotel");
+                SendSmsNotificationToSlack::dispatch("Error delivering SMS to Ringotel")->onQueue('messages');
                 return false;
             })
             ->json();
@@ -62,8 +65,10 @@ class CommioInboundSMS extends Model
                 $message->status = 'success';
             } elseif (isset($response['error'])) {
                 $message->status = json_encode($response['error']);
+                SendSmsNotificationToSlack::dispatch("Ringotel API Error: " . $message->status .". Commio Inbound SMS from " . $message->source . " to " . $message->destination )->onQueue('messages');
             } else {
                 $message->status = 'unknown';
+                SendSmsNotificationToSlack::dispatch("Ringotel API Unknown Error. Commio Inbound SMS from " . $message->source . " to " . $message->destination )->onQueue('messages');
             }
             $message->save();
         }
