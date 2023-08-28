0d23a6b0ba82d14b0e9e31a2e92324626431f4e8
nemerald-voip <info@nemerald.com>
2023-08-27 23:57:58 -0700

    attempting to create custom filters

--- resources/views/layouts/cdrs/call-category-filter.blade.php
+++ resources/views/layouts/cdrs/call-category-filter.blade.php
@@ -1,12 +1,30 @@
-<div class="dropdown">
-    <button class="btn btn-light dropdown-toggle" type="button" id="dropdownMenuButton" data-bs-toggle="dropdown"
-        aria-haspopup="true" aria-expanded="false">
-        Filter
+<div>
+    <button class="btn btn-link mb-2" type="button" data-bs-toggle="collapse" data-bs-target="#filterCollapse"
+        aria-expanded="false" aria-controls="filterCollapse">
+        Filters
+        @if ($count = $component->getFilterBadgeCount())
+            <span class="badge bg-info">
+                {{ $count }}
+            </span>
+        @endif
+        <i class="mdi mdi-chevron-down d-none d-sm-inline-block align-middle"></i>
     </button>
-    <div class="dropdown-menu " aria-labelledby="dropdownMenuButton">
-            {{-- <div class="card">
-                <div class="card-body"> --}}
-                    {{-- <form class="px-4 py-3" wire:submit.prevent="applyFilters">
+    <div class="collapse" id="filterCollapse">
+
+        <div class="mb-3">
+            <label class="form-label">Date Range</label>
+            <input type="text" class="form-control date" id="dateRangeFilter" data-toggle="date-picker" data-cancel-class="btn-warning">
+        </div> 
+
+
+        {{-- <div class="card card-body"> --}}
+            <div class="dropdown">
+                <button class="btn btn-light dropdown-toggle" type="button" id="dropdownMenuButton"
+                    data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    
+                </button>
+                <div class="dropdown-menu " aria-labelledby="dropdownMenuButton">
+                    <form class="px-4 py-3" wire:submit.prevent="applyFilters">
                         <div class="form-group">
                             <input type="text" class="form-control" id="searchInput" placeholder="Search...">
                         </div>
@@ -34,28 +52,32 @@
                                 <button type="submit" class="btn btn-primary">Apply</button>
                             </div>
                         </div>
-                    </form> --}}
-
-                    <form class="p-3">
-                        <div class="mb-3">
-                            <label for="exampleDropdownFormEmail1" class="form-label">Email address</label>
-                            <input type="email" class="form-control" id="exampleDropdownFormEmail1" placeholder="email@example.com">
-                        </div>
-                        <div class="mb-3">
-                            <label for="exampleDropdownFormPassword1" class="form-label">Password</label>
-                            <input type="password" class="form-control" id="exampleDropdownFormPassword1" placeholder="Password">
-                        </div>
-                        <div class="mb-3">
-                            <div class="form-check">
-                                <input type="checkbox" class="form-check-input" id="dropdownCheck">
-                                <label class="form-check-label" for="dropdownCheck">
-                                    Remember me
-                                </label>
-                            </div>
-                        </div>
-                        <button type="submit" class="btn btn-primary">Sign in</button>
                     </form>
-                {{-- </div>
-            </div> --}}
+
+                </div>
+            </div>
+        {{-- </div> --}}
     </div>
 </div>
+
+
+@push('scripts')
+<script>
+    $(document).ready(function() {
+
+        $('#dateRangeFilter').daterangepicker({
+            timePicker: false,
+            startDate: moment(),
+            endDate: moment(),
+            locale: {
+                format: 'MM/DD/YY'
+            }
+        }).on('apply.daterangepicker', function(e) {
+            var location = window.location.protocol +"//" + window.location.host + window.location.pathname;
+            location += '?page=1&' + $('#filterForm').serialize();
+            window.location.href = location;
+        });
+    });
+
+</script>
+@endpush
