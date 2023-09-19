b78d661e0c71bf8b75050db261b5e55a7be5b8c3
Alexander S <alexander@sharapov.biz>
2023-09-19 22:07:24 +0600

    Minor fixes of ring groups

--- resources/views/layouts/partials/greetingSelector.blade.php
+++ resources/views/layouts/partials/greetingSelector.blade.php
@@ -6,12 +6,12 @@
                 data-placeholder="Choose ..."
                 id="{{$id}}"
                 name="{{$id}}">
-            <option value=""></option>
+            <option value="disabled">Disabled</option>
             @if (!$allRecordings->isEmpty())
                 <optgroup label="Recordings">
                     @foreach ($allRecordings as $recording)
-                        <option value="{{ $recording->recording_name }}"
-                                @if($recording->recording_name == $ringGroup->ring_group_greeting)
+                        <option value="{{ $recording->recording_filename }}"
+                                @if($recording->recording_filename == $value)
                                     selected
                                 @endif>
                             {{ $recording->recording_name }}
@@ -21,34 +21,50 @@
             @endif
         </select>
     </div>
-    <div>
+    <div id="{{$id}}_play_pause_wrapper" @if($value == null) class="d-none" @endif>
         <button type="button" class="btn btn-light" id="{{$id}}_play_button" title="Play"><i class="uil uil-play"></i> </button>
         <button type="button" class="btn btn-light" id="{{$id}}_pause_button" title="Pause"><i class="uil uil-pause"></i> </button>
     </div>
     <audio id="{{$id}}_audio_file" @if ($value) src="{{ route('getRecordings', ['filename' => $value] ) }}" @endif ></audio>
 </div>
+@if($hint ?? false)
+    <span class="help-block"><small>{{$hint}}</small></span>
+@endif
 <div id="{{$id}}_err" class="text-danger error_message"></div>
 @push('scripts')
     <script>
         $(document).ready(function () {
+            const greetingBaseUrl = '{{ route('getRecordings', ['filename' => '/'] ) }}/';
             const greetingPauseButton = $('#{{$id}}_pause_button');
             const greetingPlayButton = $('#{{$id}}_play_button');
-            const audioElement = document.getElementById('{{$id}}_audio_file');
-            greetingPauseButton.hide();
-            greetingPlayButton.click(function(){
-                $(this).hide();
-                greetingPauseButton.show();
-                audioElement.play();
-                audioElement.addEventListener('ended', function() {
-                    greetingPauseButton.hide();
-                    greetingPlayButton.show();
+            const greetingPlayPauseWrapper = $('#{{$id}}_play_pause_wrapper');
+            initGreetingPreview(document.getElementById('{{$id}}_audio_file'));
+            $('#{{$id}}').on('change', function (e) {
+                if(e.target.value === '' || e.target.value === 'disabled') {
+                    greetingPlayPauseWrapper.addClass('d-none');
+                } else {
+                    greetingPlayPauseWrapper.removeClass('d-none');
+                    document.getElementById('{{$id}}_audio_file').setAttribute('src', greetingBaseUrl+e.target.value);
+                    initGreetingPreview(document.getElementById('{{$id}}_audio_file'));
+                }
+            })
+            function initGreetingPreview(audioElement) {
+                greetingPauseButton.hide();
+                greetingPlayButton.click(function(){
+                    $(this).hide();
+                    greetingPauseButton.show();
+                    audioElement.play();
+                    audioElement.addEventListener('ended', function() {
+                        greetingPauseButton.hide();
+                        greetingPlayButton.show();
+                    });
                 });
-            });
-            greetingPauseButton.click(function(){
-                $(this).hide();
-                greetingPlayButton.show();
-                audioElement.pause();
-            });
+                greetingPauseButton.click(function(){
+                    $(this).hide();
+                    greetingPlayButton.show();
+                    audioElement.pause();
+                });
+            }
         });
     </script>
 @endpush
