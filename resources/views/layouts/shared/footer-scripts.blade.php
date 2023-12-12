2091902fb0faecf09d111de8f2eb59e696c84f59
nemerald-voip <info@nemerald.com>
2023-12-12 10:58:01 -0800

    select2 theme fix

--- resources/views/layouts/shared/footer-scripts.blade.php
+++ resources/views/layouts/shared/footer-scripts.blade.php
@@ -5,10 +5,10 @@
 
 @vite([
     "node_modules/select2/dist/css/select2.min.css",
-    "resources/scss/app-modern.scss",
-    "resources/scss/icons.scss",
     'resources/js/ui/component.toastr.js',
     'node_modules/jquery-toast-plugin/dist/jquery.toast.min.css', 
+    "resources/scss/app-modern.scss",
+    "resources/scss/icons.scss",
     ])
 
 
