7750609913b332cf21a6ecc616694aa711a479e3
Nemerald <info@nemerald.com>
2024-01-19 22:07:01 -0800

    added new scripts

--- install/queue_server.sh
+++ install/queue_server.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-sudo apt-get install php7.4-mbstring
-sudo apt install imagemagick php7.4-imagick
-sudo apt-get install php7.4-zip
-service  php7.4-fpm restart
-echo "PHP dependencies are installed\n\n"
-
 sudo apt-get -y install supervisor
 echo "Supervisor is installed\n\n"
 
@@ -17,7 +11,8 @@ echo "Redis Server is installed\n\n"
 
 cp install/horizon.conf /etc/supervisor/conf.d/
 
-composer require laravel/horizon:*
+php artisan vendor:publish --provider="Laravel\Horizon\HorizonServiceProvider"
+
 
 sudo supervisorctl reread
 sudo supervisorctl update
