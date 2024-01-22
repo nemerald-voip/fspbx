ade143a3843d2d9c3b1a1922e0d249dc8797b51f
nemerald-voip <info@nemerald.com>
2024-01-22 15:57:16 -0800

    added plain text email part

--- app/Mail/AppCredentials.php
+++ app/Mail/AppCredentials.php
@@ -7,6 +7,7 @@ use Illuminate\Mail\Mailable;
 use App\Models\DefaultSettings;
 use Symfony\Component\Mime\Email;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Mail\Mailables\Content;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use SimpleSoftwareIO\QrCode\Facades\QrCode;
@@ -24,12 +25,12 @@ class AppCredentials extends Mailable
      */
     public function __construct($attributes)
     {
-        $settings = DefaultSettings::where('default_setting_category','email')->get();
+        $settings = DefaultSettings::where('default_setting_category', 'email')->get();
         if ($settings) {
             foreach ($settings as $setting) {
                 if ($setting->default_setting_subcategory == "smtp_from") {
                     $attributes['unsubscribe_email'] = $setting->default_setting_value;
-                } 
+                }
                 if ($setting->default_setting_subcategory == "support_email") {
                     $attributes['support_email'] = $setting->default_setting_value;
                 }
@@ -53,11 +54,23 @@ class AppCredentials extends Mailable
         });
 
         $this->attributes['qrCode'] = QrCode::format('png')
-        ->generate('{"domain":"' . $this->attributes["domain"] . '","username":"' . $this->attributes["username"] . '","password":"' . $this->attributes["password"] . '"}');
+            ->generate('{"domain":"' . $this->attributes["domain"] . '","username":"' . $this->attributes["username"] . '","password":"' . $this->attributes["password"] . '"}');
 
         // logger($this->attributes['qrCode']);
 
 
-        return $this->subject(config('app.name', 'Laravel') . ' App Credentials')->view('emails.app.credentials');
+        // return $this->subject(config('app.name', 'Laravel') . ' App Credentials')->view('emails.app.credentials');
+        return $this->subject(config('app.name', 'Laravel') . ' App Credentials');
+    }
+
+    /**
+     * Get the message content definition.
+     */
+    public function content(): Content
+    {
+        return new Content(
+            view: 'emails.app.credentials',
+            text: 'emails.app.credentials-text'
+        );
     }
 }
