f346fe746573773d48571bc3d09bc05d4f6492b5
Alexander S. <alexander@sharapov.biz>
2024-02-12 12:08:05 +0600

    Making progress on devices page

--- resources/js/Pages/devices.vue
+++ resources/js/Pages/devices.vue
@@ -195,6 +195,14 @@
                 @update:show="editModalTrigger = false"
             />
         </template>
+        <template #modal-action-buttons>
+            <button type="button"
+                    class="inline-flex w-full justify-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 sm:col-start-2"
+                    @click="handleSave" ref="saveButtonRef">Save</button>
+            <button type="button"
+                    class="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:col-start-1 sm:mt-0"
+                    @click="editModalTrigger = false" ref="cancelButtonRef">Cancel</button>
+        </template>
     </AddEditItemModal>
 </template>
 
