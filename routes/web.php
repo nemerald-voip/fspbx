01220d03566e541df9e8487c85990b370a37c762
nemerald-voip <info@nemerald.com>
2024-03-13 09:57:14 -0700

    improvements to routes

--- routes/web.php
+++ routes/web.php
@@ -35,74 +35,17 @@ use App\Http\Controllers\VoicemailMessagesController;
 |
 */
 
-Route::get('/extensions/callerid', [ExtensionsController::class, 'callerID'])->withoutMiddleware(['auth','web']) ->name('callerID');
-Route::post('/extensions/{extension}/callerid/update/', [ExtensionsController::class, 'updateCallerID'])->withoutMiddleware(['auth','web']) ->name('updateCallerID');
+Route::get('/extensions/callerid', [ExtensionsController::class, 'callerID'])->withoutMiddleware(['auth', 'web'])->name('callerID');
+Route::post('/extensions/{extension}/callerid/update/', [ExtensionsController::class, 'updateCallerID'])->withoutMiddleware(['auth', 'web'])->name('updateCallerID');
 
 //Polycom log handling
-Route::put('/polycom/log/{name}', [PolycomLogController::class, 'store'])->withoutMiddleware(['auth','web']) ->name('log.store');
-Route::get('/polycom/log/{name}', [PolycomLogController::class, 'show'])->withoutMiddleware(['auth','web']) ->name('log.get');
+Route::put('/polycom/log/{name}', [PolycomLogController::class, 'store'])->withoutMiddleware(['auth', 'web'])->name('log.store');
+Route::get('/polycom/log/{name}', [PolycomLogController::class, 'show'])->withoutMiddleware(['auth', 'web'])->name('log.get');
 // Route::get('/extensions', [ExtensionsController::class, 'index']) ->name('extensionsList');
 
-// Extensions
-Route::resource('extensions', ExtensionsController::class);
-Route::post('/extensions/import',[ExtensionsController::class, 'import']) ->name('extensions.import');
-Route::post('/extensions/{extension}/assign-device', [ExtensionsController::class, 'assignDevice'])->name('extensions.assign-device');
-Route::delete('/extensions/{extension}/unassign/{deviceLine}/device', [ExtensionsController::class, 'unAssignDevice'])->name('extensions.unassign-device');
-Route::delete('/extensions/{extension}/callforward/{type}', [ExtensionsController::class, 'clearCallforwardDestination'])->name('extensions.clear-callforward-destination');
-Route::post('/extensions/{extension}/send-event-notify', [ExtensionsController::class, 'sendEventNotify'])->name('extensions.send-event-notify');
-Route::post('/extensions/send-event-notify-all', [ExtensionsController::class, 'sendEventNotifyAll'])->name('extensions.send-event-notify-all');
-
-// Call Detail Records
-Route::get('/call-detail-records', [CdrsController::class, 'index'])->name('cdrs.index');
-Route::get('/call-detail-records/file/{filePath}/{fileName}', [CdrsController::class, 'serveRecording'])->name('serve.recording');
-
-
-// Groups
-Route::resource('groups', GroupsController::class);
-
-//Domains
-Route::get('domains/extensions', [DomainController::class, 'countExtensionsInDomains']);
-
-//Fax
-Route::resource('faxes', FaxesController::class);
-Route::get('/faxes/newfax/create', [FaxesController::class, 'new']) ->name('faxes.newfax');
-Route::get('/faxes/inbox/{id}', [FaxesController::class, 'inbox']) ->name('faxes.inbox.list');
-Route::get('/faxes/sent/{id}', [FaxesController::class, 'sent']) ->name('faxes.sent.list');
-Route::get('/faxes/active/{id}', [FaxesController::class, 'active']) ->name('faxes.active.list');
-Route::get('/faxes/log/{id}', [FaxesController::class, 'log']) ->name('faxes.log.list');
-Route::delete('/faxes/deleteSentFax/{id}', [FaxesController::class, 'deleteSentFax']) ->name('faxes.file.deleteSentFax');
-Route::delete('/faxes/deleteReceivedFax/{id}', [FaxesController::class, 'deleteReceivedFax']) ->name('faxes.file.deleteReceivedFax');
-Route::delete('/faxes/deleteFaxLog/{id}', [FaxesController::class, 'deleteFaxLog']) ->name('faxes.file.deleteFaxLog');
-Route::get('/fax/inbox/{file}/download', [FaxesController::class, 'downloadInboxFaxFile']) ->name('downloadInboxFaxFile');
-Route::get('/fax/sent/{file}/download', [FaxesController::class, 'downloadSentFaxFile']) ->name('downloadSentFaxFile');
-Route::get('/fax/sent/{faxQueue}/{status?}', [FaxesController::class, 'updateStatus'])->name('faxes.file.updateStatus');
-Route::post('/faxes/send', [FaxesController::class, 'sendFax']) -> name ('faxes.sendFax');
-
-// Domain Groups
-Route::resource('domaingroups', DomainGroupsController::class);
-
-// Voicemail Messages
-Route::get('/voicemails/{voicemail}/messages/', [VoicemailMessagesController::class, 'index']) ->name('voicemails.messages.index');
-Route::delete('/voicemails/messages/{message}', [VoicemailMessagesController::class, 'destroy']) ->name('voicemails.messages.destroy');
-Route::get('/voicemails/messages/{message}', [VoicemailMessagesController::class, 'getVoicemailMessage']) ->name('getVoicemailMessage');
-Route::get('/voicemails/messages/{message}/download', [VoicemailMessagesController::class, 'downloadVoicemailMessage']) ->name('downloadVoicemailMessage');
-Route::get('/voicemails/messages/{message}/delete', [VoicemailMessagesController::class, 'deleteVoicemailMessage']) ->name('deleteVoicemailMessage');
-
-
-// SIP Credentials
-Route::get('/extensions/{extension}/sip/show', [ExtensionsController::class, 'sipShow']) ->name('extensions.sip.show');
-
 // Webhooks
-Route::webhooks('webhook/postmark','postmark');
-Route::webhooks('webhook/commio/sms','commio_messaging');
-
-//Users
-Route::resource('users', UsersController::class);
-
-Route::resource('voicemails', VoicemailController::class);
-
-Route::post('user/{user}/settings', [UserSettingsController::class, 'store'])->name('users.settings.store');
-Route::delete('user/settings/{setting}', [UserSettingsController::class, 'destroy'])->name('users.settings.destroy');
+Route::webhooks('webhook/postmark', 'postmark');
+Route::webhooks('webhook/commio/sms', 'commio_messaging');
 
 // Route::get('preview-email', function () {
 //     $markdown = new \Illuminate\Mail\Markdown(view(), config('mail.markdown'));
@@ -110,7 +53,65 @@ Route::delete('user/settings/{setting}', [UserSettingsController::class, 'destro
 //     return $markdown->render("emails.app.credentials");
 //    });
 
-Route::group(['middleware' => 'auth'], function(){
+Route::group(['middleware' => 'auth'], function () {
+
+    // Extensions
+    Route::resource('extensions', ExtensionsController::class);
+    Route::post('/extensions/import', [ExtensionsController::class, 'import'])->name('extensions.import');
+    Route::post('/extensions/{extension}/assign-device', [ExtensionsController::class, 'assignDevice'])->name('extensions.assign-device');
+    Route::delete('/extensions/{extension}/unassign/{deviceLine}/device', [ExtensionsController::class, 'unAssignDevice'])->name('extensions.unassign-device');
+    Route::delete('/extensions/{extension}/callforward/{type}', [ExtensionsController::class, 'clearCallforwardDestination'])->name('extensions.clear-callforward-destination');
+    Route::post('/extensions/{extension}/send-event-notify', [ExtensionsController::class, 'sendEventNotify'])->name('extensions.send-event-notify');
+    Route::post('/extensions/send-event-notify-all', [ExtensionsController::class, 'sendEventNotifyAll'])->name('extensions.send-event-notify-all');
+
+    // Call Detail Records
+    Route::get('/call-detail-records', [CdrsController::class, 'index'])->name('cdrs.index');
+    Route::get('/call-detail-records/file/{filePath}/{fileName}', [CdrsController::class, 'serveRecording'])->name('serve.recording');
+
+    //Domains
+    Route::get('domains/extensions', [DomainController::class, 'countExtensionsInDomains']);
+
+    //Users
+    Route::resource('users', UsersController::class);
+
+    Route::post('user/{user}/settings', [UserSettingsController::class, 'store'])->name('users.settings.store');
+    Route::delete('user/settings/{setting}', [UserSettingsController::class, 'destroy'])->name('users.settings.destroy');
+
+    // Groups
+    Route::resource('groups', GroupsController::class);
+
+    //Fax
+    Route::resource('faxes', FaxesController::class);
+    Route::get('/faxes/newfax/create', [FaxesController::class, 'new'])->name('faxes.newfax');
+    Route::get('/faxes/inbox/{id}', [FaxesController::class, 'inbox'])->name('faxes.inbox.list');
+    Route::get('/faxes/sent/{id}', [FaxesController::class, 'sent'])->name('faxes.sent.list');
+    Route::get('/faxes/active/{id}', [FaxesController::class, 'active'])->name('faxes.active.list');
+    Route::get('/faxes/log/{id}', [FaxesController::class, 'log'])->name('faxes.log.list');
+    Route::delete('/faxes/deleteSentFax/{id}', [FaxesController::class, 'deleteSentFax'])->name('faxes.file.deleteSentFax');
+    Route::delete('/faxes/deleteReceivedFax/{id}', [FaxesController::class, 'deleteReceivedFax'])->name('faxes.file.deleteReceivedFax');
+    Route::delete('/faxes/deleteFaxLog/{id}', [FaxesController::class, 'deleteFaxLog'])->name('faxes.file.deleteFaxLog');
+    Route::get('/fax/inbox/{file}/download', [FaxesController::class, 'downloadInboxFaxFile'])->name('downloadInboxFaxFile');
+    Route::get('/fax/sent/{file}/download', [FaxesController::class, 'downloadSentFaxFile'])->name('downloadSentFaxFile');
+    Route::get('/fax/sent/{faxQueue}/{status?}', [FaxesController::class, 'updateStatus'])->name('faxes.file.updateStatus');
+    Route::post('/faxes/send', [FaxesController::class, 'sendFax'])->name('faxes.sendFax');
+
+    // Domain Groups
+    Route::resource('domaingroups', DomainGroupsController::class);
+
+    //Voicemails
+    Route::resource('voicemails', VoicemailController::class);
+
+    // Voicemail Messages
+    Route::get('/voicemails/{voicemail}/messages/', [VoicemailMessagesController::class, 'index'])->name('voicemails.messages.index');
+    Route::delete('/voicemails/messages/{message}', [VoicemailMessagesController::class, 'destroy'])->name('voicemails.messages.destroy');
+    Route::get('/voicemails/messages/{message}', [VoicemailMessagesController::class, 'getVoicemailMessage'])->name('getVoicemailMessage');
+    Route::get('/voicemails/messages/{message}/download', [VoicemailMessagesController::class, 'downloadVoicemailMessage'])->name('downloadVoicemailMessage');
+    Route::get('/voicemails/messages/{message}/delete', [VoicemailMessagesController::class, 'deleteVoicemailMessage'])->name('deleteVoicemailMessage');
+
+
+    // SIP Credentials
+    Route::get('/extensions/{extension}/sip/show', [ExtensionsController::class, 'sipShow'])->name('extensions.sip.show');
+
     // Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
     Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
     Route::get('/logout', [App\Http\Controllers\Auth\LoginController::class, 'logout']);
@@ -129,44 +130,44 @@ Route::group(['middleware' => 'auth'], function(){
     //Route::get('/users/edit/{id}', [UsersController::class, 'editUser']) ->name('editUser');
     // Route::post('/saveUser', [UsersController::class, 'saveUser']) ->name('saveUser');
     // Route::post('/updateUser', [UsersController::class, 'updateUser']) ->name('updateUser');
-    Route::post('/deleteUser', [UsersController::class, 'deleteUser']) ->name('deleteUser');
-    Route::post('/addSetting', [UsersController::class, 'addSetting']) ->name('addSetting');
+    Route::post('/deleteUser', [UsersController::class, 'deleteUser'])->name('deleteUser');
+    Route::post('/addSetting', [UsersController::class, 'addSetting'])->name('addSetting');
 
 
     //Voicemails
-    Route::post('/voicemails/greetings/upload/{voicemail}', [VoicemailController::class, 'uploadVoicemailGreeting']) ->name('uploadVoicemailGreeting');
-    Route::get('/voicemails/{voicemail}/greetings/{filename}', [VoicemailController::class, 'getVoicemailGreeting']) ->name('getVoicemailGreeting');
-    Route::get('/voicemails/{voicemail}/greetings/{filename}/download', [VoicemailController::class, 'downloadVoicemailGreeting']) ->name('downloadVoicemailGreeting');
-    Route::get('/voicemails/{voicemail}/greetings/{filename}/delete', [VoicemailController::class, 'deleteVoicemailGreeting']) ->name('deleteVoicemailGreeting');
+    Route::post('/voicemails/greetings/upload/{voicemail}', [VoicemailController::class, 'uploadVoicemailGreeting'])->name('uploadVoicemailGreeting');
+    Route::get('/voicemails/{voicemail}/greetings/{filename}', [VoicemailController::class, 'getVoicemailGreeting'])->name('getVoicemailGreeting');
+    Route::get('/voicemails/{voicemail}/greetings/{filename}/download', [VoicemailController::class, 'downloadVoicemailGreeting'])->name('downloadVoicemailGreeting');
+    Route::get('/voicemails/{voicemail}/greetings/{filename}/delete', [VoicemailController::class, 'deleteVoicemailGreeting'])->name('deleteVoicemailGreeting');
 
     //Apps
-    Route::get('/apps', [AppsController::class, 'index']) ->name('appsStatus');
-    Route::post('/apps/organization/create', [AppsController::class, 'createOrganization']) ->name('appsCreateOrganization');
-    Route::delete('/apps/organization/{domain}', [AppsController::class, 'destroyOrganization']) ->name('appsDestroyOrganization');
-    Route::get('/apps/organization/', [AppsController::class, 'getOrganizations']) ->name('appsGetOrganizations');
-    Route::post('/apps/organization/sync', [AppsController::class, 'syncOrganizations']) ->name('appsSyncOrganizations');
-    Route::post('/apps/users/{extension}', [AppsController::class, 'mobileAppUserSettings']) ->name('mobileAppUserSettings');
+    Route::get('/apps', [AppsController::class, 'index'])->name('appsStatus');
+    Route::post('/apps/organization/create', [AppsController::class, 'createOrganization'])->name('appsCreateOrganization');
+    Route::delete('/apps/organization/{domain}', [AppsController::class, 'destroyOrganization'])->name('appsDestroyOrganization');
+    Route::get('/apps/organization/', [AppsController::class, 'getOrganizations'])->name('appsGetOrganizations');
+    Route::post('/apps/organization/sync', [AppsController::class, 'syncOrganizations'])->name('appsSyncOrganizations');
+    Route::post('/apps/users/{extension}', [AppsController::class, 'mobileAppUserSettings'])->name('mobileAppUserSettings');
     //Route::get('/apps/organization/update', [AppsController::class, 'updateOrganization']) ->name('appsUpdateOrganization');
-    Route::post('/apps/connection/create', [AppsController::class, 'createConnection']) ->name('appsCreateConnection');
-    Route::get('/apps/connection/update', [AppsController::class, 'updateConnection']) ->name('appsUpdateConnection');
-    Route::post('/apps/user/create', [AppsController::class, 'createUser']) ->name('appsCreateUser');
-    Route::post('/apps/{domain}/user/sync', [AppsController::class, 'syncUsers']) ->name('appsSyncUsers');
-    Route::delete('/apps/users/{extension}', [AppsController::class, 'deleteUser']) ->name('appsDeleteUser');
-    Route::post('/apps/users/{extension}/resetpassword', [AppsController::class, 'ResetPassword']) ->name('appsResetPassword');
-    Route::post('/apps/users/{extension}/status', [AppsController::class, 'SetStatus']) ->name('appsSetStatus');
-    Route::get('/apps/email', [AppsController::class, 'emailUser']) ->name('emailUser');
+    Route::post('/apps/connection/create', [AppsController::class, 'createConnection'])->name('appsCreateConnection');
+    Route::get('/apps/connection/update', [AppsController::class, 'updateConnection'])->name('appsUpdateConnection');
+    Route::post('/apps/user/create', [AppsController::class, 'createUser'])->name('appsCreateUser');
+    Route::post('/apps/{domain}/user/sync', [AppsController::class, 'syncUsers'])->name('appsSyncUsers');
+    Route::delete('/apps/users/{extension}', [AppsController::class, 'deleteUser'])->name('appsDeleteUser');
+    Route::post('/apps/users/{extension}/resetpassword', [AppsController::class, 'ResetPassword'])->name('appsResetPassword');
+    Route::post('/apps/users/{extension}/status', [AppsController::class, 'SetStatus'])->name('appsSetStatus');
+    Route::get('/apps/email', [AppsController::class, 'emailUser'])->name('emailUser');
 
     // Contacts
-    Route::get('/contacts', [ContactsController::class, 'index']) ->name('contacts.list');
-    Route::delete('/contacts/{id}',[ContactsController::class, 'destroy'])->name('contacts.destroy');
-    Route::post('/contacts/import',[ContactsController::class, 'import']) ->name('contacts.import');
+    Route::get('/contacts', [ContactsController::class, 'index'])->name('contacts.list');
+    Route::delete('/contacts/{id}', [ContactsController::class, 'destroy'])->name('contacts.destroy');
+    Route::post('/contacts/import', [ContactsController::class, 'import'])->name('contacts.import');
 
 
     // SMS for testing
     // Route::get('/sms/ringotelwebhook', [SmsWebhookController::class,"messageFromRingotel"]);
 
     // Messages
-    Route::get('/messages', [MessagesController::class, 'index']) ->name('messagesStatus');
+    Route::get('/messages', [MessagesController::class, 'index'])->name('messagesStatus');
 
     // Email Queues
     Route::get('emailqueue', [EmailQueueController::class, 'index'])->name('emailqueue.list');
@@ -174,8 +175,8 @@ Route::group(['middleware' => 'auth'], function(){
     Route::get('emailqueue/{emailQueue}/{status?}', [EmailQueueController::class, 'updateStatus'])->name('emailqueue.updateStatus');
 
     // Fax Queue
-    Route::get('faxqueue',[FaxQueueController::class, 'index'])->name('faxQueue.list');
-    Route::delete('faxqueue/{id}',[FaxQueueController::class, 'destroy'])->name('faxQueue.destroy');
+    Route::get('faxqueue', [FaxQueueController::class, 'index'])->name('faxQueue.list');
+    Route::delete('faxqueue/{id}', [FaxQueueController::class, 'destroy'])->name('faxQueue.destroy');
     Route::get('faxqueue/{faxQueue}/{status?}', [FaxQueueController::class, 'updateStatus'])->name('faxQueue.updateStatus');
 
     // Ring Groups
@@ -183,11 +184,11 @@ Route::group(['middleware' => 'auth'], function(){
 
     // Recordings
     Route::get('recordings', [RecordingsController::class, 'index'])->name('recordings.index');
-    Route::get('recordings/i-{recording}',[RecordingsController::class, 'show'])->name('recordings.show');
+    Route::get('recordings/i-{recording}', [RecordingsController::class, 'show'])->name('recordings.show');
     Route::get('recordings/{filename}', [RecordingsController::class, 'file'])->name('recordings.file');
-    Route::delete('recordings/{recording}',[RecordingsController::class, 'destroy'])->name('recordings.destroy');
-    Route::post('recordings',[RecordingsController::class, 'store'])->name('recordings.store');
-    Route::post('recordings/storeBlob',[RecordingsController::class, 'storeBlob'])->name('recordings.storeBlob');
+    Route::delete('recordings/{recording}', [RecordingsController::class, 'destroy'])->name('recordings.destroy');
+    Route::post('recordings', [RecordingsController::class, 'store'])->name('recordings.store');
+    Route::post('recordings/storeBlob', [RecordingsController::class, 'storeBlob'])->name('recordings.storeBlob');
     Route::put('recordings/{recording}', [RecordingsController::class, 'update'])->name('recordings.update');
     Route::put('recordings/{recording}/{entity}/{entityid}', [RecordingsController::class, 'use'])->name('recordings.use');
 
