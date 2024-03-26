8ba7f4228a72c4262d6cc744ed53e062507a53be
nemerald-voip <info@nemerald.com>
2024-03-26 08:44:40 -0700

    bug fix

--- routes/web.php
+++ routes/web.php
@@ -61,8 +61,11 @@ Route::group(['middleware' => 'auth'], function () {
 
     // Extensions
     Route::resource('extensions', ExtensionsController::class);
-    Route::post('/extensions/import', [ExtensionsController::class, 'import'])->name('extensions.import');
+    Route::post('/extensions/import',[ExtensionsController::class, 'import']) ->name('extensions.import');
     Route::post('/extensions/{extension}/assign-device', [ExtensionsController::class, 'assignDevice'])->name('extensions.assign-device');
+    Route::post('/extensions/{extension}/device', [ExtensionsController::class, 'oldStoreDevice'])->name('extensions.store-device');
+    Route::get('/extensions/{extension}/device/{device}/edit', [ExtensionsController::class, 'oldEditDevice'])->name('extensions.edit-device');
+    Route::put('/extensions/{extension}/device/{device}', [ExtensionsController::class, 'oldUpdateDevice'])->name('extensions.update-device');
     Route::delete('/extensions/{extension}/unassign/{deviceLine}/device', [ExtensionsController::class, 'unAssignDevice'])->name('extensions.unassign-device');
     Route::delete('/extensions/{extension}/callforward/{type}', [ExtensionsController::class, 'clearCallforwardDestination'])->name('extensions.clear-callforward-destination');
     Route::post('/extensions/{extension}/send-event-notify', [ExtensionsController::class, 'sendEventNotify'])->name('extensions.send-event-notify');
@@ -70,6 +73,7 @@ Route::group(['middleware' => 'auth'], function () {
 
     // Call Detail Records
     Route::get('/call-detail-records', [CdrsController::class, 'index'])->name('cdrs.index');
+    Route::post('/call-detail-records', [CdrsController::class, 'index'])->name('cdrs.download');
     Route::get('/call-detail-records/file/{filePath}/{fileName}', [CdrsController::class, 'serveRecording'])->name('serve.recording');
 
     //Domains
@@ -86,18 +90,18 @@ Route::group(['middleware' => 'auth'], function () {
 
     //Fax
     Route::resource('faxes', FaxesController::class);
-    Route::get('/faxes/newfax/create', [FaxesController::class, 'new'])->name('faxes.newfax');
-    Route::get('/faxes/inbox/{id}', [FaxesController::class, 'inbox'])->name('faxes.inbox.list');
-    Route::get('/faxes/sent/{id}', [FaxesController::class, 'sent'])->name('faxes.sent.list');
-    Route::get('/faxes/active/{id}', [FaxesController::class, 'active'])->name('faxes.active.list');
-    Route::get('/faxes/log/{id}', [FaxesController::class, 'log'])->name('faxes.log.list');
-    Route::delete('/faxes/deleteSentFax/{id}', [FaxesController::class, 'deleteSentFax'])->name('faxes.file.deleteSentFax');
-    Route::delete('/faxes/deleteReceivedFax/{id}', [FaxesController::class, 'deleteReceivedFax'])->name('faxes.file.deleteReceivedFax');
-    Route::delete('/faxes/deleteFaxLog/{id}', [FaxesController::class, 'deleteFaxLog'])->name('faxes.file.deleteFaxLog');
-    Route::get('/fax/inbox/{file}/download', [FaxesController::class, 'downloadInboxFaxFile'])->name('downloadInboxFaxFile');
-    Route::get('/fax/sent/{file}/download', [FaxesController::class, 'downloadSentFaxFile'])->name('downloadSentFaxFile');
+    Route::get('/faxes/newfax/create', [FaxesController::class, 'new']) ->name('faxes.newfax');
+    Route::get('/faxes/inbox/{id}', [FaxesController::class, 'inbox']) ->name('faxes.inbox.list');
+    Route::get('/faxes/sent/{id}', [FaxesController::class, 'sent']) ->name('faxes.sent.list');
+    Route::get('/faxes/active/{id}', [FaxesController::class, 'active']) ->name('faxes.active.list');
+    Route::get('/faxes/log/{id}', [FaxesController::class, 'log']) ->name('faxes.log.list');
+    Route::delete('/faxes/deleteSentFax/{id}', [FaxesController::class, 'deleteSentFax']) ->name('faxes.file.deleteSentFax');
+    Route::delete('/faxes/deleteReceivedFax/{id}', [FaxesController::class, 'deleteReceivedFax']) ->name('faxes.file.deleteReceivedFax');
+    Route::delete('/faxes/deleteFaxLog/{id}', [FaxesController::class, 'deleteFaxLog']) ->name('faxes.file.deleteFaxLog');
+    Route::get('/fax/inbox/{file}/download', [FaxesController::class, 'downloadInboxFaxFile']) ->name('downloadInboxFaxFile');
+    Route::get('/fax/sent/{file}/download', [FaxesController::class, 'downloadSentFaxFile']) ->name('downloadSentFaxFile');
     Route::get('/fax/sent/{faxQueue}/{status?}', [FaxesController::class, 'updateStatus'])->name('faxes.file.updateStatus');
-    Route::post('/faxes/send', [FaxesController::class, 'sendFax'])->name('faxes.sendFax');
+    Route::post('/faxes/send', [FaxesController::class, 'sendFax']) -> name ('faxes.sendFax');
 
     // Domain Groups
     Route::resource('domaingroups', DomainGroupsController::class);
