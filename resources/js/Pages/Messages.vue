b72cde8fc94e94422504b16ac828ea8b9eb7b6a3
nemerald-voip <info@nemerald.com>
2024-05-02 11:12:51 -0700

    layout adjustment

--- resources/js/Pages/Messages.vue
+++ resources/js/Pages/Messages.vue
@@ -131,7 +131,7 @@
                     <TableField class="whitespace-nowrap px-2 py-2 text-sm text-gray-500"
                         :text="row.destination" />
 
-                    <TableField class="whitespace-nowrap px-2 py-2 text-sm text-gray-500"
+                    <TableField class=" px-2 py-2 text-sm text-gray-500"
                         :text="row.message" />
                         <TableField class="whitespace-nowrap px-2 py-2 text-sm text-gray-500"
                         :text="row.type" />
