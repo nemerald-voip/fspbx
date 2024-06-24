2d567f3f3d8c3fa3fbbd3c3533bb12ccbbd37f04
nemerald-voip <info@nemerald.com>
2024-06-24 08:53:23 -0700

    updated styling to fix apperiance in Gmail

--- resources/views/emails/export/export-completed.blade.php
+++ resources/views/emails/export/export-completed.blade.php
@@ -17,7 +17,9 @@
     <table border="0" cellpadding="0" cellspacing="0" role="presentation">
     <tr>
     <td>
-    <a href="{{ $attributes['fileUrl'] }}" class="button button-primary" target="_blank" rel="noopener">Download report</a>
+        <a href="{{ $attributes['fileUrl'] }}" target="_blank" rel="noopener" style="display: inline-block; padding: 10px 20px; font-size: 16px; color: #ffffff; background-color: #4a90e2; border-radius: 5px; text-decoration: none;">
+            Download report
+        </a>
     </td>
     </tr>
     </table>
