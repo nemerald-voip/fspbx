15e272f1b164c5d9ac3802323e4ab25fcd1eb865
nemerald-voip <info@nemerald.com>
2024-06-27 00:03:26 -0700

    added reports page

--- app/Http/Controllers/ReportsController.php
+++ app/Http/Controllers/ReportsController.php
@@ -2,28 +2,15 @@
 
 namespace App\Http\Controllers;
 
-use Exception;
 use Inertia\Inertia;
-use App\Models\Messages;
-use App\Models\Extensions;
-use Illuminate\Http\Request;
-use App\Models\DomainSettings;
-use App\Models\SmsDestinations;
-use Illuminate\Support\Facades\Http;
-use App\Services\SynchMessageProvider;
-use App\Services\CommioMessageProvider;
-use Illuminate\Support\Facades\Session;
-use App\Jobs\SendSmsNotificationToSlack;
+use App\Models\Domain;
+use App\Jobs\ExportReport;
 
 class ReportsController extends Controller
 {
 
-    public $model;
-    public $filters = [];
-    public $sortField;
-    public $sortOrder;
+
     protected $viewName = 'Reports';
-    protected $searchable = [];
 
     // public function __construct()
     // {
@@ -41,19 +28,100 @@ class ReportsController extends Controller
         return Inertia::render(
             $this->viewName,
             [
-                // 'data' => function () {
-                //     return $this->getData();
-                // },
+                'data' => function () {
+                    return $this->getData();
+                },
+                'viewName' => function () {
+                    return $this->viewName;
+                },
                 // 'showGlobal' => function () {
                 //     return request('filterData.showGlobal') === 'true';
                 // },
 
                 'routes' => [
                     'current_page' => route('reports.index'),
+                    'generate' => route('reports.generate'),
                 ]
             ]
         );
     }
 
 
+    /**
+     *  Get data
+     */
+    public function getData()
+    {
+
+        $data = collect([
+            ['reportName' => 'Active and suspended extensions per domain'],
+
+        ]);
+
+        return $data;
+    }
+
+
+
+    public function store()
+    {
+
+        logger(request('reportName'));
+        try {
+
+            if (request('reportName') == "Active and suspended extensions per domain") {
+
+                $domains = Domain::select(
+                    'domain_uuid',
+                    'domain_name',
+                    'domain_description',
+                )
+                    ->with(['extensions' => function ($query) {
+                        $query->select('extension_uuid', 'domain_uuid');
+                    }])
+                    ->get();
+            }
+
+            // Iterate through the collection to count extensions and suspended extensions
+            $domainData = $domains->map(function ($domain) {
+                $totalExtensions = $domain->extensions->count();
+                $suspendedExtensions = $domain->extensions->where('suspended', true)->count();
+                $activeExtensions = $totalExtensions - $suspendedExtensions;
+
+                return [
+                    'domain_uuid' => $domain->domain_uuid,
+                    'domain_name' => $domain->domain_name,
+                    'domain_description' => $domain->domain_description,
+                    'total_extensions' => $totalExtensions,
+                    'suspended_extensions' => $suspendedExtensions,
+                    'active_extensions' => $activeExtensions,
+                ];
+            });
+
+            // logger($domainData);
+
+            $params['user_email'] = auth()->user()->user_email;
+
+            // $cdrs = $this->getData(false); // returns lazy collection
+
+            ExportReport::dispatch($params,$domainData);
+
+            // Return a JSON response indicating success
+            return response()->json([
+                'messages' => ['success' => ['Report is being generated in the background. We\'ll email you a link when it\'s ready to download.']],
+            ], 200);
+        } catch (\Exception $e) {
+            logger($e->getMessage());
+            // Handle any other exception that may occur
+            return response()->json([
+                'success' => false,
+                'errors' => ['server' => ['Failed to export items']]
+            ], 500); // 500 Internal Server Error for any other errors
+        }
+
+        return response()->json([
+            'success' => false,
+            'errors' => ['server' => ['Failed to export']]
+        ], 500); // 500 Internal Server Error for any other errors
+    }
 }
