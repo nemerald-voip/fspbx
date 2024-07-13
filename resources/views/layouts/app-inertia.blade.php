e32a8d64f86fa5f25dbc78c202a22b0bc6137fb3
nemerald-voip <info@nemerald.com>
2024-07-13 00:02:49 -0700

    cleaned up old code

--- resources/views/layouts/app-inertia.blade.php
+++ resources/views/layouts/app-inertia.blade.php
@@ -1,43 +1,12 @@
-{{-- @extends('layouts/app')
-
-@push('head.end')
-    @vite(['resources/js/vue.js'])
-    @vite(['resources/scss/tailwind.css'])
-    @inertiaHead
-@endpush
-
-@php
-$hideHorizontalNav = true;
-@endphp
-
-
-@section('content')
-    @inertia
-@endsection --}}
-
-
 
 <!doctype html>
-@if (request()->is('contact-center*'))
-    <html lang="{{ str_replace('_', '-', app()->getLocale()) }}" data-layout-mode="detached" data-topbar-color="dark"
-        data-menu-color="light" data-sidenav-user="true" data-sidenav-size="0px">
-@endif
-
-@if (!request()->is('contact-center*'))
-    <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-@endif
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
 <head>
     @include('layouts.shared/title-meta', ['title' => $title ?? null])
     @yield('css')
-    @if (request()->is('contact-center*'))
-        @include('layouts.shared/head-css', ['mode' => $mode ?? '', 'demo' => $demo ?? ''])
-        @vite(['resources/js/app.js', 'resources/js/hyper-head.js', 'resources/js/hyper-config.js'])
-    @endif
 
-    @if (!request()->is('contact-center*'))
-        @vite(['resources/scss/tailwind.css'])
-    @endif
+    @vite(['resources/scss/tailwind.css'])
 
     @vite(['resources/js/vue.js'])
 
@@ -46,46 +15,6 @@ $hideHorizontalNav = true;
     @inertiaHead
 </head>
 
-
-@if (request()->is('contact-center*'))
-    <div class="wrapper">
-
-        @auth
-
-            <div class="content-page">
-                <div class="content">
-
-
-                    @include('layouts.shared/horizontal-nav')
-
-                    {{-- @dd(Session::all()); --}}
-
-                    @yield('content')
-                    @inertia
-
-                </div>
-
-                @include('layouts.shared/footer')
-
-            </div>
-        @else
-            @yield('content')
-        @endauth
-
-
-
-    </div>
-
-    @auth
-        @include('layouts.shared/right-sidebar')
-        @yield('modal')
-
-    @endauth
-
-    @include('layouts.shared/footer-scripts')
-    @vite(['resources/js/hyper-main.js'])
-    </body>
-@endif
 
 @if (request()->is('login'))
 
@@ -115,7 +44,7 @@ $hideHorizontalNav = true;
 @endif
 
 
-@if (!request()->is('contact-center*') && !request()->is('login'))
+@if ( !request()->is('login'))
 
     <body class="m-0 font-nunito text-gray-600 bg-gray-100">
         <div class="max-w-95 mx-auto">
