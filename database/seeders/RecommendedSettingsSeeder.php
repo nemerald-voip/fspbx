14cde76548044f04eea7dfea3353c3d0a70381a3
nemerald-voip <info@nemerald.com>
2024-12-05 22:16:16 -0800

    added dependency

--- database/seeders/RecommendedSettingsSeeder.php
+++ database/seeders/RecommendedSettingsSeeder.php
@@ -2,8 +2,9 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\DefaultSettings;
 use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 class RecommendedSettingsSeeder extends Seeder
 {
