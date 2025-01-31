479144f78192b109a855992a9884fa49e69eee6a
nemerald-voip <info@nemerald.com>
2025-01-30 23:50:08 -0800

    finished table

--- resources/js/Pages/FaxQueue.vue
+++ resources/js/Pages/FaxQueue.vue
@@ -16,17 +16,15 @@
                         <Spinner class="ml-1" :show="isExporting" />
                     </button> -->
 
-                    <!-- <button v-if="!showGlobal && page.props.auth.can.cdrs_view_global" type="button"
-                        @click.prevent="handleShowGlobal()"
+                    <button v-if="!filterData.showGlobal" type="button" @click.prevent="handleShowGlobal()"
                         class="rounded-md bg-white px-2.5 py-1.5 ml-2 sm:ml-4 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
                         Show global
                     </button>
 
-                    <button v-if="showGlobal && page.props.auth.can.cdrs_view_global" type="button"
-                        @click.prevent="handleShowLocal()"
+                    <button v-if="filterData.showGlobal" type="button" @click.prevent="handleShowLocal()"
                         class="rounded-md bg-white px-2.5 py-1.5 ml-2 sm:ml-4 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
                         Show local
-                    </button> -->
+                    </button>
 
                 </template>
 
@@ -97,7 +95,7 @@
                 </template>
 
                 <template v-if="selectPageItems" v-slot:current-selection>
-                    <td colspan="6">
+                    <td colspan="10">
                         <div class="text-sm text-center m-2">
                             <span class="font-semibold ">{{ selectedItems.length }} </span> items are selected.
                             <button v-if="!selectAll && selectedItems.length != data.total"
@@ -117,29 +115,57 @@
                 <template #table-body>
                     <tr v-for="row in data.data" :key="row.fax_queue_uuid">
 
-                        <TableField class="whitespace-nowrap px-4 py-2 text-sm text-gray-500" :text="row.fax_caller_id_number">
-                        <div class="flex items-center">
-                            <input v-if="row.fax_queue_uuid" v-model="selectedItems" type="checkbox" name="action_box[]"
-                                :value="row.fax_queue_uuid" class="h-4 w-4 rounded border-gray-300 text-indigo-600">
-                            <div class="ml-9"
-                                :class="{ 'cursor-pointer hover:text-gray-900': page.props.auth.can.voicemail_update, }"
-                                @click="page.props.auth.can.voicemail_update && handleEditRequest(row.fax_queue_uuid)">
-                                <span class="flex items-center">
-                                    <UserIcon class="mr-2 h-5 w-5 text-indigo-500" />
-                                    {{ row.fax_caller_id_number }}
-                                </span>
+                        <TableField class="whitespace-nowrap px-4 py-2 text-sm text-gray-500"
+                            :text="row.fax_caller_id_number">
+                            <div class="flex items-center">
+                                <input v-if="row.fax_queue_uuid" v-model="selectedItems" type="checkbox" name="action_box[]"
+                                    :value="row.fax_queue_uuid" class="h-4 w-4 rounded border-gray-300 text-indigo-600">
+                                <div class="ml-9"
+                                    :class="{ 'cursor-pointer hover:text-gray-900': page.props.auth.can.voicemail_update, }"
+                                    @click="page.props.auth.can.voicemail_update && handleEditRequest(row.fax_queue_uuid)">
+                                    <span class="flex items-center">
+                                        {{ row.fax_caller_id_number }}
+                                    </span>
+                                </div>
                             </div>
-                        </div>
-                    </TableField>
+                        </TableField>
 
                         <TableField class="whitespace-nowrap px-2 py-2 text-sm text-gray-500" :text="row.fax_number" />
                         <TableField class="whitespace-nowrap px-2 py-2 text-sm text-gray-500"
                             :text="row.fax_email_address" />
                         <TableField class="whitespace-nowrap px-2 py-2 text-sm text-gray-500" :text="row.fax_date" />
-                        <TableField class="whitespace-nowrap px-2 py-2 text-sm text-gray-500" :text="row.fax_status" />
+                        <TableField class="whitespace-nowrap px-2 py-2 text-sm text-gray-500" :text="row.fax_status">
+                            <Badge :text="row.fax_status" :backgroundColor="determineColor(row.fax_status).backgroundColor"
+                                :textColor="determineColor(row.fax_status).textColor"
+                                :ringColor="determineColor(row.fax_status).ringColor" />
+
+                        </TableField>
                         <TableField class="whitespace-nowrap px-2 py-2 text-sm text-gray-500" :text="row.fax_retry_date" />
                         <TableField class="whitespace-nowrap px-2 py-2 text-sm text-gray-500" :text="row.fax_retry_count" />
                         <TableField class="whitespace-nowrap px-2 py-2 text-sm text-gray-500" :text="row.fax_notify_date" />
+
+                        <TableField class="whitespace-nowrap px-2 py-1 text-sm text-gray-500">
+                            <template #action-buttons>
+                                <div class="flex items-center whitespace-nowrap">
+                                    <!-- <ejs-tooltip v-if="page.props.auth.can.device_update" :content="'Edit'" position='TopCenter'
+                                    target="#destination_tooltip_target">
+                                    <div id="destination_tooltip_target">
+                                        <PencilSquareIcon @click="handleEditRequest(row.device_uuid)"
+                                            class="h-9 w-9 transition duration-500 ease-in-out py-2 rounded-full text-gray-400 hover:bg-gray-200 hover:text-gray-600 active:bg-gray-300 active:duration-150 cursor-pointer" />
+
+                                    </div>
+                                </ejs-tooltip> -->
+
+                                    <ejs-tooltip :content="'Retry'" position='TopCenter' target="#restart_tooltip_target">
+                                        <div id="restart_tooltip_target">
+                                            <RestartIcon @click="handleRetry(row.message_uuid)"
+                                                class="h-9 w-9 transition duration-500 ease-in-out py-2 rounded-full text-gray-400 hover:bg-gray-200 hover:text-gray-600 active:bg-gray-300 active:duration-150 cursor-pointer" />
+                                        </div>
+                                    </ejs-tooltip>
+
+                                </div>
+                            </template>
+                        </TableField>
                     </tr>
                 </template>
                 <template #empty>
@@ -169,10 +195,6 @@
     </MainLayout>
 
 
-    <CallDetailsModal :show="viewModalTrigger" :item="itemData" :loading="loadingModal" :customClass="'sm:max-w-4xl'"
-        @close="handleModalClose">
-    </CallDetailsModal>
-
     <Notification :show="notificationShow" :type="notificationType" :messages="notificationMessages"
         @update:show="hideNotification" />
 </template>
@@ -189,12 +211,15 @@ import Paginator from "./components/general/Paginator.vue";
 import moment from 'moment-timezone';
 import { registerLicense } from '@syncfusion/ej2-base';
 import DatePicker from "./components/general/DatePicker.vue";
-import CallDetailsModal from "./components/modal/CallDetailsModal.vue"
 import Notification from "./components/notifications/Notification.vue";
 import {
     MagnifyingGlassIcon,
 } from "@heroicons/vue/24/solid";
-import { DocumentArrowDownIcon } from "@heroicons/vue/24/outline";
+import BulkActionButton from "./components/general/BulkActionButton.vue";
+import RestartIcon from "./components/icons/RestartIcon.vue";
+import { TooltipComponent as EjsTooltip } from "@syncfusion/ej2-vue-popups";
+import Badge from "./components/general/Badge.vue";
+
 
 import {
     startOfDay, endOfDay,
@@ -209,21 +234,16 @@ const loadingModal = ref(false)
 const notificationType = ref(null);
 const notificationMessages = ref(null);
 const notificationShow = ref(null);
-const isExporting = ref(null);
 const selectedItems = ref([]);
 const selectPageItems = ref(false);
-
+const selectAll = ref(false);
 
 const props = defineProps({
     data: Object,
-    showGlobal: Boolean,
     startPeriod: String,
     endPeriod: String,
-    search: String,
     timezone: String,
-    csvUrl: Object,
     routes: Object,
-    itemData: Object,
     statusOptions: Object,
 });
 
@@ -238,12 +258,22 @@ onMounted(() => {
 
 const filterData = ref({
     search: props.search,
-    showGlobal: props.showGlobal,
+    showGlobal: false,
     dateRange: [moment.tz(props.startPeriod, props.timezone).startOf('day').format(), moment.tz(props.endPeriod, props.timezone).endOf('day').format()],
     // dateRange: ['2024-07-01T00:00:00', '2024-07-01T23:59:59'],
     timezone: props.timezone,
 
 });
+
+const handleShowGlobal = () => {
+    filterData.value.showGlobal = true;
+    handleSearchButtonClick();
+}
+
+const handleShowLocal = () => {
+    filterData.value.showGlobal = false;
+    handleSearchButtonClick();
+}
 
 const handleSearchButtonClick = () => {
     loading.value = true;
@@ -303,7 +333,7 @@ const bulkActions = computed(() => {
         {
             id: 'bulk_retry',
             label: 'Retry',
-            icon: 'PencilSquareIcon'
+            icon: 'RestartIcon'
         }
     ];
 
@@ -329,29 +359,19 @@ const handleUpdateDateRange = (newDateRange) => {
     filterData.value.dateRange = newDateRange;
 }
 
-const exportCsv = () => {
-    isExporting.value = true;
-
-    axios.post(props.routes.export, {
-        filterData: filterData._rawValue,
-    })
-        .then(response => {
+const handleSelectAll = () => {
+    axios.post(props.routes.select_all, filterData._rawValue)
+        .then((response) => {
+            selectedItems.value = response.data.items;
+            selectAll.value = true;
             showNotification('success', response.data.messages);
-            isExporting.value = false;
-        })
-        .catch(error => {
-            console.error('There was an error with the request:', error);
-            isExporting.value = false;
+
+        }).catch((error) => {
+            handleClearSelection();
             handleErrorResponse(error);
         });
 
-
 };
-
-
-const handleModalClose = () => {
-    viewModalTrigger.value = false;
-}
 
 const hideNotification = () => {
     notificationShow.value = false;
@@ -386,13 +406,54 @@ const handleErrorResponse = (error) => {
 
 const handleSelectPageItems = () => {
     if (selectPageItems.value) {
-        selectedItems.value = props.data.data.map(item => item.voicemail_uuid);
+        selectedItems.value = props.data.data.map(item => item.fax_queue_uuid);
     } else {
         selectedItems.value = [];
     }
 };
 
+const determineColor = (status) => {
+    switch (status) {
+        case 'sent':
+            return {
+                backgroundColor: 'bg-green-50',
+                textColor: 'text-green-700',
+                ringColor: 'ring-green-600/20'
+            };
+        case 'sending':
+            return {
+                backgroundColor: 'bg-blue-50',
+                textColor: 'text-blue-700',
+                ringColor: 'ring-blue-600/20'
+            };
+        case 'trying':
+            return {
+                backgroundColor: 'bg-cyan-50',
+                textColor: 'text-cyan-700',
+                ringColor: 'ring-cyan-600/20'
+            };
+        case 'failed':
+            return {
+                backgroundColor: 'bg-rose-50',
+                textColor: 'text-rose-700',
+                ringColor: 'ring-rose-600/20'
+            };
+        default:
+            return {
+                backgroundColor: 'bg-yellow-50',
+                textColor: 'text-yellow-700',
+                ringColor: 'ring-yellow-600/20'
+            };
+    }
+};
+
+registerLicense('Ngo9BigBOggjHTQxAR8/V1NAaF5cWWdCf1FpRmJGdld5fUVHYVZUTXxaS00DNHVRdkdnWX5eeHVSQ2hYUkB3WEI=');
 
 
 </script>
 
+
+<style>
+@import "@syncfusion/ej2-base/styles/tailwind.css";
+@import "@syncfusion/ej2-vue-popups/styles/tailwind.css";
+</style>
