dc701fdcf61e1646f7cf4536e22ff3e29c3bdf4f
nemerald <info@nemerald.com>
2025-07-22 07:23:12 +0000

    url update

--- documentation/docusaurus.config.ts
+++ documentation/docusaurus.config.ts
@@ -15,7 +15,7 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://fspbx.com',
+  url: 'https://nemerald-voip.github.io/fspbx',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
