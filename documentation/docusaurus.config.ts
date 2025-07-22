e6abbefd5ae904e8e5ce8f40d657f60e4a059b57
nemerald <info@nemerald.com>
2025-07-22 07:41:37 +0000

    base url update

--- documentation/docusaurus.config.ts
+++ documentation/docusaurus.config.ts
@@ -18,7 +18,7 @@ const config: Config = {
   url: 'https://nemerald-voip.github.io/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '',
+  baseUrl: '/fspbx/',
   trailingSlash: true,
 
   // GitHub pages deployment config.
