c33b2c35c0f1f36be1bd701fec33e1f779a14372
nemerald <info@nemerald.com>
2025-07-23 06:22:01 +0000

    updated url

--- documentation/docusaurus.config.ts
+++ documentation/docusaurus.config.ts
@@ -11,11 +11,11 @@ const config: Config = {
 
   // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
   future: {
-    v4: true, // Improve compatibility with the upcoming Docusaurus v4
+    v4: false, // Improve compatibility with the upcoming Docusaurus v4
   },
 
   // Set the production url of your site here
-  url: 'https://fspbx.com/',
+  url: 'https://www.fspbx.com/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
