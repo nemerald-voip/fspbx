8501945a949fc0103316d807168100445fc33d9c
nemerald <info@nemerald.com>
2025-07-23 07:00:11 +0000

    cleaned up code

--- documentation/src/pages/index.tsx
+++ documentation/src/pages/index.tsx
@@ -3,37 +3,16 @@ import clsx from 'clsx';
 import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
-import HomepageFeatures from '@site/src/components/HomepageFeatures';
 import Heading from '@theme/Heading';
 import { Carousel } from 'react-responsive-carousel';
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
-import { Dialog, DialogPanel, Disclosure, DisclosureButton, DisclosurePanel } from '@headlessui/react'
+import { Disclosure, DisclosureButton, DisclosurePanel } from '@headlessui/react'
 import { LockClosedIcon, CheckIcon } from '@heroicons/react/20/solid'
 import { ComputerDesktopIcon, BoltIcon, CpuChipIcon, PuzzlePieceIcon, ServerStackIcon, PlusIcon, MinusIcon } from "@heroicons/react/24/outline";
 
 
 import styles from './index.module.css';
 
-function HomepageHeader() {
-    const { siteConfig } = useDocusaurusContext();
-    return (
-        <header className={clsx('hero hero--primary', styles.heroBanner)}>
-            <div className="container">
-                <Heading as="h1" className="hero__title">
-                    {siteConfig.title}
-                </Heading>
-                <p className="hero__subtitle">{siteConfig.tagline}</p>
-                <div className={styles.buttons}>
-                    <Link
-                        className="button button--secondary button--lg"
-                        to="/docs/intro">
-                        Docusaurus Tutorial - 5min ⏱️
-                    </Link>
-                </div>
-            </div>
-        </header>
-    );
-}
 
 export default function Home(): ReactNode {
     const { siteConfig } = useDocusaurusContext();
