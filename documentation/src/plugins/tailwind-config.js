935ff49b7ca364967054ab8798e43d606e8b7bd2
nemerald <info@nemerald.com>
2025-10-11 22:14:15 +0000

    fixed styling

--- documentation/src/plugins/tailwind-config.js
+++ documentation/src/plugins/tailwind-config.js
@@ -2,7 +2,7 @@ module.exports = function tailwindPlugin(context, options) {
     return {
       name: "tailwind-plugin",
       configurePostCss(postcssOptions) {
-        postcssOptions.plugins = [require("@tailwindcss/postcss")];
+        postcssOptions.plugins.push(require("@tailwindcss/postcss"));
         return postcssOptions;
       },
     };
