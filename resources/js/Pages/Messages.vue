361ab5111975bd2183794544b890a55ee0fe7cc1
nemerald <info@nemerald.com>
2026-02-18 06:39:30 +0000

    update

--- resources/js/Pages/Messages.vue
+++ resources/js/Pages/Messages.vue
@@ -5,7 +5,6 @@
         <vue-advanced-chat :height="height" :current-user-id="currentUserId" :rooms="JSON.stringify(rooms)"
             :messages="JSON.stringify(messages)" :loading-rooms="loadingRooms" :loading-messages="loadingMessages"
             :rooms-loaded="roomsLoaded" :messages-loaded="messagesLoaded" :room-id="currentRoomId"
-            :show-add-room="showAddRoom"
             @fetch-rooms="fetchRooms" @fetch-messages="fetchMessages"  @send-message="onSendMessage" />
     </div>
 
@@ -57,7 +56,6 @@ const loadingRooms = ref(false)
 const loadingMessages = ref(false)
 const roomsLoaded = ref(false)
 const messagesLoaded = ref(false)
-const showAddRoom = ref(false)
 
 // Realtime subscription handle
 let activeChannelName = null
