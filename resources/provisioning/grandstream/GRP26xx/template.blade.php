2b9f6ba9af94d4df3d0a7ebcbe31e0ed9b19a367
nemerald <info@nemerald.com>
2026-06-17 07:11:58 +0000

    updated default ringtone

--- resources/provisioning/grandstream/GRP26xx/template.blade.php
+++ resources/provisioning/grandstream/GRP26xx/template.blade.php
@@ -1,4 +1,4 @@
-{{-- version: 1.0.7 --}}
+{{-- version: 1.0.8 --}}
 
 @switch($flavor)
 
@@ -1616,7 +1616,7 @@
 		<!-- # System Ringtone -->
 		<!-- # String -->
 		<!-- # Mandatory -->
-		<item name="audio.tone.systemRing">f1=440,f2=480,c=200/400;</item>
+		<item name="audio.tone.systemRing">f1=540,f2=516,c=70/16-55/16-70/300;</item>
 
 		<!-- # Dial Tone -->
 		<!-- # String -->
