e6e887c4e987bb5ca9751384fbab03b0617a9b94
Lantizia <s.maddox@lantizia.me.uk>
2026-06-22 08:58:05 +0100

    Update docusaurus.config.ts
    Fix the "Edit this page" hyperlink at the bottom of each page of the documenation.

--- documentation/docusaurus.config.ts
+++ documentation/docusaurus.config.ts
@@ -48,7 +48,7 @@ const config: Config = {
                     // Please change this to your repo.
                     // Remove this to remove the "edit this page" links.
                     editUrl:
-                        'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+                        'https://github.com/nemerald-voip/fspbx/tree/main/documentation/',
                 },
                 blog: {
                     showReadingTime: true,
@@ -59,7 +59,7 @@ const config: Config = {
                     // Please change this to your repo.
                     // Remove this to remove the "edit this page" links.
                     editUrl:
-                        'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+                        'https://github.com/nemerald-voip/fspbx/tree/main/documentation/',
                     // Useful options to enforce blogging best practices
                     onInlineTags: 'warn',
                     onInlineAuthors: 'warn',
